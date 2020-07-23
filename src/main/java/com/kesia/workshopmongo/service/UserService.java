2f56943c19c7b38ed5723f64af11533c0471c58e
Kesia23 <kesiasilvanunes@hotmail.com>
2020-07-23 14:08:55 -0400

    Obtendo um usuário por id

--- src/main/java/com/kesia/workshopmongo/service/UserService.java
+++ src/main/java/com/kesia/workshopmongo/service/UserService.java
@@ -1,12 +1,14 @@
 package com.kesia.workshopmongo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kesia.workshopmongo.domain.User;
 import com.kesia.workshopmongo.repository.UserRepository;
+import com.kesia.workshopmongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,4 +19,9 @@ public class UserService {
 	public List<User> findAll(){
 		return repo.findAll();
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
 }
