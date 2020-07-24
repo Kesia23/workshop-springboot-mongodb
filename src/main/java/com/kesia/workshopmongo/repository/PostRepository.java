e2244a9346dfc118f255c2dc7c3f2a8e455a6552
Kesia23 <kesiasilvanunes@hotmail.com>
2020-07-24 17:59:20 -0400

    Criando entity Post com User aninhado

---
+++ src/main/java/com/kesia/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.kesia.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.kesia.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+	
+}
